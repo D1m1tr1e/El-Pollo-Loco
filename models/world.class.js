aa041ca8243758565db72b4fb8756aede96bdc7c
D1m1tr1e <126110046+D1m1tr1e@users.noreply.github.com>
2023-08-21 22:22:45 +0200

    addad redraw function for canvas and added enemies

--- models/world.class.js
+++ models/world.class.js
@@ -5,21 +5,38 @@ class World {
         new Chicken(),
         new Chicken(),
     ];
-
+    canvas;
     ctx;
 
     constructor(canvas) {
         this.ctx = ctx = canvas.getContext('2d');
+        this.canvas = canvas;
         this.draw();
-
     }
 
+
     draw() {
+        //resetet/löscht mein Cnavas
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+
+        //zeichnet mir meinen Character rein
         this.ctx.drawImage(
-            this.character.img,
-            this.character.x,
-            this.character.y,
-            this.character.width,
-            this.character.height);
+            this.character.img, this.character.x,
+            this.character.y, this.character.width, this.character.height);
+
+        // zeichnet mir meine Gegener rein
+        for (let i = 0; i < this.enemies.length; i++) {
+            let enemy = this.enemies[i];
+            this.ctx.drawImage(
+                enemy.img, enemy.x,
+                enemy.y, enemy.width, enemy.height)
+        }
+
+        //Draw() wid immer weider aufgerufen
+        //Hier wird eine self Variable definiert weil im späteren Code this nicht als this erkannt werden kann
+        let self = this;
+        requestAnimationFrame(function () {
+            self.draw();
+        });
     }
 }
