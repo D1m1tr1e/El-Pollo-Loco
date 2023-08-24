899996b7faad4e535506844c989748c257efc0c0
D1m1tr1e <126110046+D1m1tr1e@users.noreply.github.com>
2023-08-24 22:01:35 +0200

    added animation for the cloads

--- models/cload.class.js
+++ models/cload.class.js
@@ -7,5 +7,18 @@ class Cload extends MoveableObject {
 
     constructor() {
         super().loadImage('img/5_background/layers/4_clouds/1.png');
+
+        this.animateCloads()
+    }
+
+    animateCloads() {
+        setInterval(() => {
+            this.x -= 0.15;
+        }, 1000 / 60);
     }
 }
+
+
+
+
+
