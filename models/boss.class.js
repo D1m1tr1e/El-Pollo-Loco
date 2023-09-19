2e3c5a56a999efb98c2fc2c02438696cda039100
D1m1tr1e <126110046+D1m1tr1e@users.noreply.github.com>
2023-09-19 22:07:08 +0200

    work started on the animation of the boss

--- models/boss.class.js
+++ models/boss.class.js
@@ -14,18 +14,22 @@ class Boss extends MoveableObject {
         'img/4_enemie_boss_chicken/2_alert/G11.png',
         'img/4_enemie_boss_chicken/2_alert/G12.png',
     ];
+    BOSS_FIGHT_SOUND = new Audio('audio/boss_musik.mp3');
 
     constructor() {
         super().loadImage(this.IMAGES_WALKING[0]);
         this.loadImages(this.IMAGES_WALKING);
-        this.animateChicken();
+        this.animateBoss();
     }
 
-    animateChicken() {
-
+    animateBoss() {
+        if (this.x <= 2000) {
+            console.log('character hat den boss erreicht',this.x)
+      
         setInterval(() => {
             // walk animation
             this.playAnimation(this.IMAGES_WALKING);
         }, 300);
     }
+}
 }
--- models/coin.class.js
+++ models/coin.class.js
@@ -1,10 +1,26 @@
 class Coin extends MoveableObject {
 
+    IMAGES_GLOSSY_COINS =
+    [
+        'img/8_coin/coin_1.png',
+        'img/8_coin/coin_2.png'
+    ];
+    currentImage = 0;
+     
     constructor(imagePath, x, y, height, width) {
         super().loadImage(imagePath);
+        this.loadImages(this.IMAGES_GLOSSY_COINS);
         this.x = x;
         this.y = y;
         this.height = height;
         this.width = width;
+        this.animateCoins();
     }
+
+    animateCoins() {
+        setInterval(() => {
+            this.playAnimation(this.IMAGES_GLOSSY_COINS);
+        }, 1000/3);
+    }
+
 }
