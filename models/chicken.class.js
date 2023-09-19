cca793ad6d842f6e6871e072224cc5d4e20e9c7c
D1m1tr1e <126110046+D1m1tr1e@users.noreply.github.com>
2023-09-19 22:08:10 +0200

    work started on the animation of the boss

--- models/chicken.class.js
+++ models/chicken.class.js
@@ -12,7 +12,6 @@ class Chicken extends MoveableObject {
     ];
   
 
-
     constructor() {
         super().loadImage('img/3_enemies_chicken/chicken_normal/1_walk/2_w.png');
         this.loadImages(this.IMAGES_WALKING);
