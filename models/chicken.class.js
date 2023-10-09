1e29686a4163279b908427e2b82e2baca00057c3
D1m1tr1e <126110046+D1m1tr1e@users.noreply.github.com>
2023-10-09 21:46:43 +0200

    Update chicken.class.js

--- models/chicken.class.js
+++ models/chicken.class.js
@@ -6,6 +6,12 @@ class Chicken extends MoveableObject {
     width = 90;
     chickenKilled = false;
     killAnimationStarted = false;
+    offset = {
+        top: 0,
+        bottom: 0,
+        left: 0,
+        right: 0
+    }
     IMAGES_WALKING = [
         'img/3_enemies_chicken/chicken_normal/1_walk/1_w.png',
         'img/3_enemies_chicken/chicken_normal/1_walk/2_w.png',
