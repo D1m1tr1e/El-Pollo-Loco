423c56da6c68b949b57f7a50defde79cefcfc509
D1m1tr1e <126110046+D1m1tr1e@users.noreply.github.com>
2023-10-17 21:51:30 +0200

    #

--- models/level.class.js
+++ models/level.class.js
@@ -4,7 +4,7 @@ class Level {
     landscape;
     bottles;
     coins;
-    level_end_x = 2100;
+    level_end_x = 2200;
 
     constructor(enemies, clouds, landscape, bottles, coins) {
         this.enemies = enemies;
